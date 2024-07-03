82231e58a401ea6c4a07f6e80d066bd1887053cc
rifkigg <rifkiadi348@gmail.com>
2024-07-03 08:07:30 +0700

    kerangka

---
+++ script.js
@@ -0,0 +1,27 @@
+// Sistem Tanggal Dan Jam
+function updateTime() {
+  const now = new Date();
+  const options = { day: "2-digit", month: "2-digit", year: "numeric" };
+  const date = now.toLocaleDateString("id-ID", options);
+  const time = now.toLocaleTimeString("id-ID");
+
+  document.getElementById("currentDate").textContent = date;
+  document.getElementById("currentTime").textContent = time;
+}
+
+updateTime();
+setInterval(updateTime, 1000); // Update setiap detik
+
+// Sistem Table
+document.addEventListener("DOMContentLoaded", function () {
+  const table = document.querySelector("table");
+  const no_data = document.querySelector('.no-data');
+
+  if (table.rows.length <= 1) {
+    // Jumlah baris tabel kurang dari atau sama dengan 1 (hanya header)
+    const message = document.createElement('p');
+    message.textContent = 'Data tidak ada';
+    message.setAttribute('colspan', '8'); // Menambahkan atribut colspan
+    no_data.appendChild(message);
+  }
+});
