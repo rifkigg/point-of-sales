974e29663523a5a73a53e60e09c714d1ca0fd0dc
rifkigg <rifkiadi348@gmail.com>
2024-07-04 10:47:35 +0700

    test

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ setInterval(updateTime, 1000); // Update setiap detik
 document.addEventListener("DOMContentLoaded", function () {
   const table = document.querySelector("table");
   const no_data = document.querySelector(".no-data");
+  const tambah = document.getElementById("tambah");
+  const kurang = document.getElementById("kurang");
 
   if (table.rows.length <= 1) {
     // Jumlah baris tabel kurang dari atau sama dengan 1 (hanya header)
@@ -24,8 +26,12 @@ document.addEventListener("DOMContentLoaded", function () {
     message.setAttribute("colspan", "8"); // Menambahkan atribut colspan
     no_data.appendChild(message);
   }
+
+  
 });
 
+
+//sistem modal
 const myModal = document.getElementById("myModal");
 const myInput = document.getElementById("myInput");
 
@@ -33,4 +39,3 @@ myModal.addEventListener("shown.bs.modal", () => {
   myInput.focus();
 });
 
-// Shortcut JS
